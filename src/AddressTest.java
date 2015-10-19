17ad730b0b7d4dedec64c1a9b8e444693ebb2699
Răzvan Ionescu <rzionescu@gmail.com>
2015-10-19 21:56:06 +0300

    first commit

---
+++ src/AddressTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created by razvan on 19/10/15.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddressTest {
+    @Test
+    public void testAddresses()
+    {
+        assertEquals("The Universe is broken", 2 + 2, 4);
+    }
+}
